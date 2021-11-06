ea285796cba900a135d475789f16de39d24ab817
Rob Cone <robcone86@gmail.com>
2021-11-06 17:18:55 -0500

    working on bug issues in my movie list

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -4,35 +4,29 @@ import { useDispatch } from 'react-redux';
 // import { BsArrowRightShort, BsTrashFill } from "react-icons/bs";
 // import Paper from '@material-ui/core/Paper';
 // import './MovieItem.css';
-
 function MovieItem({ movie }) {
 
-    //Set history to change pages.
     const history = useHistory();
-
     const dispatch = useDispatch();
 
     const toDetailsPage = () => {
         dispatch({ type: 'SET_SELECTED_MOVIE', payload: movie });
-       
         history.push('/details')
     }
 
     const removeTitle = () => {
         dispatch({ type: 'REMOVE_TITLE', payload: movie });
-   
     }
 
     return (
-
         <div key={movie.id} className="item">
             <h3 className="title">{movie.title}</h3>
-                <div className="poster">
-                    <img onClick={toDetailsPage} src={movie.poster} alt={movie.title} />
-               
+            <div className="poster">
+                <img onClick={toDetailsPage} src={movie.poster} alt={movie.title} />
+            </div>
         </div>
-
     );
 }
+
 
 export default MovieItem;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,32 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import MovieItem from '../MovieItem/MovieItem';
-// import './MovieList.css';
+import './MovieList.css';
 
-function MovieList() {
+function MovieList(){
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+const dispatch = useDispatch();
+const movies = useSelector(store => store.movies);
+const history = useHistory();
 
-    //Set history to change pages.
-    const history = useHistory();
-
-    const toAddMoviePage = () => {
-        history.push('/add')
-    }
-
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            
-            
-            </section>
-        </main>
-
-    );
 }
 
 export default MovieList;
